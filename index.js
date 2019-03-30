7017e83174af2ce5f7a9b618ee5bdf978d09aa3a
njenga <njengak@me.com>
2019-03-30 14:56:25 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+
+// let doc = document.getElementById('main');
+
+// document.body.appendChild('h1')
+
+const main = document.querySelector('main')
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerText = "She is the champion";
